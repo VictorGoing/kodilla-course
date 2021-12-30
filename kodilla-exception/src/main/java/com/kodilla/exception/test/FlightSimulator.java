0536c651f2d62b4764583b37eed49eafbdb7d6bc
WiktorIdziak <Witusaki@gmail.com>
2021-12-30 09:25:31 +0100

    8.4 FlightSimulator v2

--- kodilla-exception/src/main/java/com/kodilla/exception/test/FlightSimulator.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FlightSimulator.java
@@ -8,12 +8,14 @@ public class FlightSimulator {
 
     public void findFlight(Flight flight) throws RouteNotFoundException{
         Map<String, Boolean> airports = new HashMap<>();
-        try {
+
             Boolean bool = airports.get(flight.getArrivalAirport());
-            if(bool) System.out.println("Airport is available");
-            else System.out.println("Airport is not available");
-        } catch (NullPointerException e){
-            throw new RouteNotFoundException();
-        }
+            if(bool != null) System.out.println("Airport is available");
+            else {
+                System.out.println("Airport is not available");
+                throw new RouteNotFoundException();
+            }
+
+
     }
 }
