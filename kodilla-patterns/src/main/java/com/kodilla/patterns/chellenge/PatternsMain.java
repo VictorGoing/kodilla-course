053bfc8d38526301c922951180d0b05fb94977ef
WiktorIdziak <Witusaki@gmail.com>
2022-02-08 09:12:11 +0100

    13.1 MovieStore

---
+++ kodilla-patterns/src/main/java/com/kodilla/patterns/chellenge/PatternsMain.java
@@ -0,0 +1,10 @@
+package com.kodilla.patterns.chellenge;
+
+public class PatternsMain {
+    public static void main(String[] args) {
+
+        System.out.println("Czesc");
+        MovieStore object = new MovieStore();
+        System.out.println(object.giveMeAll());
+    }
+}
