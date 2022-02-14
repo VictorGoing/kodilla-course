649c8ca5bba434e1b7ab8c8e5c5e6ed7f52d42c6
WiktorIdziak <Witusaki@gmail.com>
2022-02-14 18:50:15 +0100

    13.5 FlightFinder

---
+++ kodilla-patterns/src/main/java/com/kodilla/patterns/chellenge/airport/FlightFinder.java
@@ -0,0 +1,37 @@
+package com.kodilla.patterns.chellenge.airport;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class FlightFinder {
+    private HashSet<Flight> flights;
+
+
+    public FlightFinder() {
+        this.flights = new HashSet<>();
+    }
+
+    public List<Flight> findFrom(String city){
+       return flights.stream().filter(x -> x.getFrom().equals(city)).collect(Collectors.toList());
+    }
+
+    public List<Flight> findTo(String city){
+        return flights.stream().filter(x -> x.getTo().equals(city)).collect(Collectors.toList());
+    }
+
+    public List<Pair<Flight,Flight>> findToConnection(String fromCity, String toCity){
+        List<Flight> fromList = findFrom(fromCity);
+        List<Flight> toList = findTo(toCity);
+        List<Pair<Flight,Flight>> resultList = new ArrayList<>();
+        for(Flight from :fromList){
+            for(Flight to :toList){
+                if(from.getTo().equals(to.getFrom())){
+                    resultList.add(new Pair<>(from,to));
+                }
+            }
+        }
+        return resultList;
+    }
+}
