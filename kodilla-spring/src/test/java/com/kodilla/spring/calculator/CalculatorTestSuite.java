5ed2c98a4f0f53b46bafb0fe77971b87b41ecaf5
WiktorIdziak <Witusaki@gmail.com>
2022-02-22 18:48:20 +0100

    14.2 CalculatorTestSuite

---
+++ kodilla-spring/src/test/java/com/kodilla/spring/calculator/CalculatorTestSuite.java
@@ -0,0 +1,19 @@
+package com.kodilla.spring.calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class CalculatorTestSuite {
+    @Autowired
+    Calculator calculator;
+
+    @Test
+    void testCalculations(){
+        calculator.add(4,2);
+        calculator.sub(4,2);
+        calculator.mul(4,2);
+        calculator.div(4,2);
+    }
+}
